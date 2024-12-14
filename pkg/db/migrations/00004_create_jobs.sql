84073197dc202603124bf235f1e446e3ce3adf3c
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-12-14 23:04:00 +0530

    Fixed GetJobs SQL query. Changed mercedes job to Dream salary tier.

--- pkg/db/migrations/00004_create_jobs.sql
+++ pkg/db/migrations/00004_create_jobs.sql
@@ -4,7 +4,7 @@ INSERT INTO job_table(company_id, job_role, ctc, salary_tier, apply_by_date, cgp
 VALUES(1, 'SDE', 24.5, 'Open Dream', '2025-06-12 15:04:05', 8.5, '{"ISE", "CSE", "CY", "DS"}'),
 (2, 'SRE', 12, 'Open Dream', '2025-07-12 15:04:05', 7.5, '{"ISE", "CSE", "CY", "DS", "ECE", "EIE", "EEE", "ETE", "CV", "ME"}'),
 (2, 'SDE', 64, 'Open Dream', '2025-03-12 15:04:05', 9, '{"ISE", "CSE", "CY", "DS"}'),
-(3, 'Mechanical Engineer', 10.5, 'Open Dream', '2025-05-12 15:04:05', 8.5, '{"ME"}');
+(3, 'Mechanical Engineer', 5.5, 'Dream', '2025-05-12 15:04:05', 8.5, '{"ME"}');
 -- +goose StatementEnd
 
 -- +goose Down
--- pkg/db/queries/job_queries.sql
+++ pkg/db/queries/job_queries.sql
@@ -1,8 +1,8 @@
 -- name: GetJobs :many
 SELECT job_id, job_role, ctc, salary_tier, apply_by_date, cgpa_cutoff, company_name, industry
-FROM job_table LEFT JOIN company_table
+FROM job_table JOIN company_table
 on job_table.company_id = company_table.company_id
-where (array_length($2::VARCHAR[], 1) = 0 OR salary_tier = ANY($2))
+where (COALESCE(array_length($2::VARCHAR[], 1), 0) = 0 OR salary_tier = ANY($2))
 and NOW() < apply_by_date
 and cgpa_cutoff <= (SELECT cgpa from student_table where student_id = $1);
 
