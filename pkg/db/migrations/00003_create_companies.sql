72e7c123270f8e921acaa9c86ba9fe7c6ced575b
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-12-15 12:30:03 +0530

    Added username, password for each company's point of contact to create job openings on the website.

--- pkg/db/migrations/00003_create_companies.sql
+++ pkg/db/migrations/00003_create_companies.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
-INSERT INTO company_table(company_name, poc_name, poc_phno, industry)
-VALUES('Microsoft', 'Harish', '1234567890', 'Software'),
-('Confluent', 'Shwetha', '9876543210', 'Software'),
-('Mercedes', 'Govind', '1122334455', 'Automobile');
+INSERT INTO company_table(company_name, poc_name, poc_phno, industry, username, password)
+VALUES('Microsoft', 'Harish', '1234567890', 'Software', 'softmicro', '1234'),
+('Confluent', 'Shwetha', '9876543210', 'Software', 'fluentcon', '1234'),
+('Mercedes', 'Govind', '1122334455', 'Automobile', 'cedesmer', '1234');
 -- +goose StatementEnd
 
 -- +goose Down
