8b42b06cc831ab587b61d1b6655bce95eb9368c5
aneeshsg <mailaneeshsg@gmail.com>
2024-12-15 17:08:06 +0530

    Partial Implementation of user registration

--- reg.go
+++ reg.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
+	"net/http"
+
+	"github.com/Adarsh-Kmt/SkillSetGo/db"
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -15,14 +20,36 @@ type User struct {
 	CounsellorName string  `json:"counsellor" binding:"required"`
 }
 
-var db *sql.DB
+var dbConn *sql.DB
 
 func main() {
 	var err error
-	connStr := "user=user password=password dbname=db sslmode=disable"
-	db, err = sql.Open("postgres", connStr)
+	connStr := "user=postgres password=@neeshpostgres dbname=db sslmode=disable"
+	dbConn, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Error: Failed to connect to database", err)
 	}
-	defer db.Close()
+	defer dbConn.Close()
+	router := mux.NewRouter()
+	router.HandleFunc("/register", registerUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+	}
+	arg := db.InsertUserParams{
+		Name:           user.Name,
+		Branch:         user.Branch,
+		Cgpa:           user.CGPA,
+		ActiveBacklogs: user.ActiveBacklogs,
+		EmailID:        user.EmailID,
+		Usn:            user.USN,
+		CounsellorName: user.CounsellorName,
+	}
 }
