c4633a67cd7931518166d620f559230f6bd62a59
aneeshsg <mailaneeshsg@gmail.com>
2024-12-28 13:34:05 +0530

    Used a map to validate the email ID entered by the user

--- pkg/entity/student_request.go
+++ pkg/entity/student_request.go
@@ -11,29 +11,47 @@ type RegisterStudentRequest struct {
 	usn    string  `json:"usn"`
 	Branch string  `json:"branch"`
 	Cgpa   float32 `json:"cgpa"`
-	Year   int     `json:"year"`
 	Email  string  `json:"email"`
 	Phone  string  `json:"phone"`
 }
 
 func ValidateRegisterStudentRequest(request RegisterStudentRequest) (httpError *util.HTTPError) {
+	branchemail := map[string]string{
+		"ISE":  ".is",
+		"CSE":  ".cs",
+		"CD":   ".cd",
+		"CY":   ".cy",
+		"AIML": ".ai",
+		"ECE":  ".ec",
+		"EEE":  ".ee",
+		"ETE":  ".et",
+		"EIE":  ".ei",
+		"ME":   ".me",
+		"CV":   ".cv",
+		"BT":   ".bt",
+		"CH":   ".ch",
+		"IEM":  ".iem",
+		"ASE":  ".ae",
+	}
 	if request.Name == "" {
 		return &util.HTTPError{StatusCode: 400, Error: "Name cannot be empty"}
 	}
 	if !strings.HasPrefix(request.usn, "1RV") {
 		return &util.HTTPError{StatusCode: 400, Error: "Invalid USN"}
 	}
-	if request.Branch == "" {
-		return &util.HTTPError{StatusCode: 400, Error: "Branch cannot be empty"}
+	substr, exists := branchemail[request.Branch]
+	if request.Branch == "" || !exists {
+		return &util.HTTPError{StatusCode: 400, Error: "Branch is empty or invalid"}
 	}
 	if request.Cgpa <= 0 || request.Cgpa > 10 {
 		return &util.HTTPError{StatusCode: 400, Error: "Invalid CGPA"}
 	}
-	if request.Year == 0 || request.Year > 4 {
-		return &util.HTTPError{StatusCode: 400, Error: "Invalid Year"}
-	}
+
 	if strings.HasSuffix(request.Email, "@rvce.edu.in") {
 		return &util.HTTPError{StatusCode: 400, Error: "Invalid Email ID"}
+	}
+	if !strings.Contains(request.Email, substr) {
+		return &util.HTTPError{StatusCode: 400, Error: "Email ID does not match branch"}
 	}
 	if request.Phone == "" || len(request.Phone) != 10 {
 		return &util.HTTPError{StatusCode: 400, Error: "Invalid Phone Number"}
