343e3dd712b7d12c4751d0e19fb9c0430bdc9526
aneeshsg <mailaneeshsg@gmail.com>
2025-01-16 23:45:46 +0530

    handler

--- pkg/handler/company_handler.go
+++ pkg/handler/company_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/adarsh-kmt/skillsetgo/pkg/entity"
 	"github.com/adarsh-kmt/skillsetgo/pkg/service"
 	"github.com/adarsh-kmt/skillsetgo/pkg/util"
 	"github.com/gorilla/mux"
@@ -24,38 +25,35 @@ func (ch *CompanyHandler) MuxSetup(mux *mux.Router) *mux.Router {
 	return mux
 }
 
-func NewCompanyHandler(cs service.CompanyService) *CompanyHandler {
-	return &CompanyHandler{cs: cs}
-}
-
-func (ch *CompanyHandler) MuxSetup(mux *mux.Router) *mux.Router {
-	mux.HandleFunc("/company", util.MakeHttpHandlerFunc(ch.GetCompanies)).Methods("GET")
-	mux.HandleFunc("/company", util.MakeHttpHandlerFunc(ch.RegisterCompany)).Methods("POST")
-	return mux
-}
-
 // RegisterCompany handles the registration of a new company
 func (ch *CompanyHandler) RegisterCompany(w http.ResponseWriter, r *http.Request) error {
-	var req service.CreateCompanyRequest
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		return util.NewHTTPError(http.StatusBadRequest, "Invalid request payload")
+	req := &entity.RegisterCompanyRequest{}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+	}
+	// arg:=sqlc.CreateCompanyParams{
+	// 	CompanyName: req.CompanyName,
+	// 	PocName: req.PocName,
+	// 	PocPhno: req.PocPhno,
+	// 	Industry:req.Industry,
+	// }
+	if httpError := entity.ValidateRegisterCompanyRequest(*req); err != nil {
+		return httpError
 	}
 
-	if err := ch.cs.RegisterCompany(r.Context(), req); err != nil {
-		return util.NewHTTPError(http.StatusInternalServerError, "Failed to register company")
-	}
-
-	util.RespondWithJSON(w, http.StatusCreated, map[string]string{"message": "Company registered successfully"})
 	return nil
 }
 
 // GetCompanies handles fetching the list of companies
 func (ch *CompanyHandler) GetCompanies(w http.ResponseWriter, r *http.Request) error {
-	companies, err := ch.cs.GetCompanies(r.Context())
-	if err != nil {
-		return util.NewHTTPError(http.StatusInternalServerError, "Failed to fetch companies")
+	request := &entity.RegisterCompanyRequest{}
+	if err := json.NewDecoder(r.Body).Decode(request); err != nil {
+		return &util.HTTPError{StatusCode: 400, Error: "bad request"}
 	}
 
-	util.RespondWithJSON(w, http.StatusOK, companies)
-	return nil
+	if httpError := entity.ValidateRegisterCompanyRequest(*request); httpError != nil {
+		return httpError
+		return nil
+	}
 }
